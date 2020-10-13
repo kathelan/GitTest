770438ed88945459c7d9d714a7ebab8302b4025d
mdeptulski <mateuszdeptulski@gmail.com>
2020-10-13 10:05:48 +0200

    initial commit

---
+++ src/main/java/com/git/test/TestApplication.java
@@ -0,0 +1,22 @@
+package com.git.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TestApplication {
+
+    @RequestMapping("/")
+    String home() {
+        System.out.println("hello");
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+}
